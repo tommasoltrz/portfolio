bc288e367bb33b061036645d40152706c07eef2a
tommasoltrz <tommasoltrz@gmail.com>
2025-01-10 10:28:11 +0100

    ✨ Update job experience descriptions in portfolio: enhance clarity and detail, improve formatting, and adjust spacing for better readability.

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
       <Separator className="mx-auto my-12" />
       <div className="grid grid-cols-1 md:grid-cols-[180px_1fr] gap-8 row-start-2 items-start">
         <h2 className="text-l font-bold">Experience</h2>
-        <div className="flex flex-col gap-12">
+        <div className="flex flex-col gap-14">
           <JobExperience
             role="Senior Frontend Engineer"
             company="Lastminute.com"
@@ -66,18 +66,17 @@ export default function Home() {
             description={
               <div className="text-muted-foreground">
                 <p>
-                  Here I&apos;m mostly responsible for{" "}
+                  Here I am a core developer for{" "}
                   <a href="https://www.weg.de" target="_blank">
                     weg.de
                   </a>{" "}
                   - a major German travel platform that helps thousands of users
                   book their trips daily. I&apos;m leading the modernization of
-                  our web platform while maintaining the current website,
-                  implementing company-wide design system standards, and
-                  building tools for monitoring performance. Key focus areas
-                  include:{" "}
+                  the web platform, implementing company-wide design system
+                  standards, and building tools for monitoring performance.{" "}
+                  <br /> Key focus areas include:{" "}
                 </p>
-                <ol className="list-disc list-inside space-y-2 my-2 ">
+                <ol className="list-disc list-inside space-y-2 mt-3 ">
                   <li>Architecting a new platform from the ground up</li>
                   <li>
                     Implementation of the design system used across multiple
@@ -107,11 +106,7 @@ export default function Home() {
             description={
               <p className="text-muted-foreground">
                 BOOM was an italian startup that aimed at transforming the world
-                of visual content management. <br />
-                Here I experienced first hand the fast-paced environment of a
-                startup. <br />
-                Unfortunately the company went bankrupt few months after I
-                joined.
+                of visual content management.
               </p>
             }
           />
@@ -124,16 +119,18 @@ export default function Home() {
             timeFrame="2018 - 2022"
             description={
               <p className=" text-muted-foreground">
-                At frog I was part of a small team of engineers building digital
-                products for some of the world&apos;s leading companies - from
-                healthcare providers to car manufacturers.
-                <br /> Being in a design consultancy meant constantly switching
-                between different challenges: one day rapidly prototyping new
-                ideas, the next leading development of production applications.{" "}
-                <br />I worked directly with stakeholders to turn their
-                ambitious visions into working solutions, while coordinating
-                teams and collaborating with some of the best designers in the
-                industry.
+                Frog is a world-leading design consultancy. Here I was part of a
+                small, high-impact engineering team developing solutions for
+                major international enterprises.
+                <br />
+                <br /> From rapidly prototyping concepts to delivering
+                production-ready applications, I worked across diverse projects
+                in healthcare, automotive, and financial sectors.
+                <br />
+                <br />I collaborated directly with stakeholders to transform
+                strategic initiatives into technical solutions, while
+                coordinating teams and working alongside industry-leading
+                designers.
               </p>
             }
           />
@@ -146,7 +143,9 @@ export default function Home() {
             location="Turin - Malmö"
             description={
               <p className="text-muted-foreground">
-                At Arduino I mostly worked on{" "}
+                Arduino is a global leader in open-source hardware and software.
+                <br />
+                Here I was the core developer of{" "}
                 <a
                   href="https://blog.arduino.cc/category/casajasmina/"
                   target="_blank"
@@ -154,14 +153,14 @@ export default function Home() {
                   Casa Jasmina
                 </a>{" "}
                 - a fully functioning open source smart home developed in
-                collaboration with sci-fi author Bruce Sterling.
-                <br /> We used it as a living lab to explore the future of
-                connected living.
+                collaboration with sci-fi author Bruce Sterling. <br />
+                We used it as a living lab to explore the future of connected
+                living.
                 <br /> <br />
-                Here I built web interfaces for IoT devices and created
-                tutorials that helped thousands of makers get started with
-                connected objects. <br /> <br />I got to wear many hats: from
-                writing documentation, libraries and tutorials of{" "}
+                I built web interfaces for IoT devices and created tutorials
+                that helped thousands of makers get started with connected
+                objects. <br /> <br />I got to wear many hats: from writing
+                documentation, libraries and tutorials of{" "}
                 <a
                   href="https://projecthub.arduino.cc/Arduino_Genuino/pavlovs-cat-296cc2"
                   target="_blank"
